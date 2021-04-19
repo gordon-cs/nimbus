289dde8a4a3bb354b2a0ad5324cbf66e12877531
Addison <abbota@vmware.com>
2021-04-19 16:35:53 -0400

    Basic options avctivity

--- app/src/main/java/com/vmware/nimbus/ui/main/OptionsActivity.java
+++ app/src/main/java/com/vmware/nimbus/ui/main/OptionsActivity.java
@@ -2,7 +2,13 @@ package com.vmware.nimbus.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.vmware.nimbus.R;
 
 public class OptionsActivity extends AppCompatActivity {
@@ -11,5 +17,19 @@ public class OptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        final Button saveButton = findViewById(R.id.save);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toastMsg("Save clicked", getApplicationContext());
+            }
+        });
+    }
+
+    public static void toastMsg(String msg, Context c) {
+        Toast toast = Toast.makeText(c, msg, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
