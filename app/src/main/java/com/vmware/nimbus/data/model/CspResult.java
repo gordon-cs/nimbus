eda2e00ce6d79e7c0b99449da9686c21bfa99ba9
Evan Duffy <evand@vmware.com>
2021-04-19 15:38:43 -0400

    Changing the Login to use the same API URL
    Make logging in and api calls use the same URL so that we can more
    easily switch vRA endpoints in the future

--- app/src/main/java/com/vmware/nimbus/data/model/CspResult.java
+++ app/src/main/java/com/vmware/nimbus/data/model/CspResult.java
@@ -2,44 +2,19 @@ package com.vmware.nimbus.data.model;
 
 public class CspResult {
 
-    private String id_token;
-    private String token_type;
-    private String scope;
-    private String access_token;
-    private String refresh_token;
+    private String tokenType;
+    private String token;
 
-    private int expires_in;
-
-    public CspResult(String id_token, String token_type, String scope, String access_token, String refresh_token, int expires_in) {
-        this.id_token = id_token;
-        this.token_type = token_type;
-        this.scope = scope;
-        this.access_token = access_token;
-        this.refresh_token = refresh_token;
-        this.expires_in = expires_in;
+    public CspResult(String tokenType, String token) {
+        this.token = token;
+        this.tokenType = tokenType;
     }
 
-    public String getId_token() {
-        return id_token;
+    public String getTokenType() {
+        return tokenType;
     }
 
-    public String getToken_type() {
-        return token_type;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public String getAccess_token() {
-        return access_token;
-    }
-
-    public String getRefresh_token() {
-        return refresh_token;
-    }
-
-    public int getExpires_in() {
-        return expires_in;
+    public String getToken() {
+        return token;
     }
 }
