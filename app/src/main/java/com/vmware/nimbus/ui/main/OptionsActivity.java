fbf3716cb6ce6273e7443fce62750a01b73350dc
Evan Duffy <evand@vmware.com>
2021-04-19 17:22:48 -0400

    Wire up BaseURL in options page so that it is stored in preferences
    Add a shared preference so that the base url can be stored and recalled
    later when making api calls

--- app/src/main/java/com/vmware/nimbus/ui/main/OptionsActivity.java
+++ app/src/main/java/com/vmware/nimbus/ui/main/OptionsActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.vmware.nimbus.R;
@@ -17,13 +20,38 @@ public class OptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        String currentBaseUrl = settings.getString(getApplicationContext().getResources().getString(R.string.base_url_shared_property_name), null);
+        if (currentBaseUrl != null && !currentBaseUrl.equals("")) {
+            ((EditText)findViewById(R.id.rootUri)).setText(currentBaseUrl);
+        }
 
         final Button saveButton = findViewById(R.id.save);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toastMsg("Save clicked", getApplicationContext());
+
+                settings.edit().putString(
+                        getApplicationContext().getResources().getString(R.string.base_url_shared_property_name),
+                        ((EditText)findViewById(R.id.rootUri)).getText().toString()
+                ).apply();
+                toastMsg("Saved Settings", getApplicationContext());
+            }
+        });
+
+        final Button restoreDefaultsButton = findViewById(R.id.restore);
+
+        restoreDefaultsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                settings.edit().remove(
+                        getApplicationContext().getResources().getString(R.string.base_url_shared_property_name)
+                ).apply();
+                ((EditText)findViewById(R.id.rootUri)).setText(getApplicationContext().getResources().getString(R.string.base_url));
+                toastMsg("Restored Settings", getApplicationContext());
             }
         });
     }
