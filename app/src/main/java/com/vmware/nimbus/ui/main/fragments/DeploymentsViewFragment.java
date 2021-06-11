77e380a003f9a20f6ee477cf8c3d4cc14d351159
Kumari Deepshikha <46506817+Deepshikha6@users.noreply.github.com>
2021-06-11 11:53:53 -0400

    Adding search for deployments and catalog items, adding padding for d… (#130)
    * Adding search for deployments and catalog items, adding padding for deployment card view
    
    Co-authored-by: kdeepshikha <kdeepshikha@vmware.com>

--- app/src/main/java/com/vmware/nimbus/ui/main/fragments/DeploymentsViewFragment.java
+++ app/src/main/java/com/vmware/nimbus/ui/main/fragments/DeploymentsViewFragment.java
@@ -1,10 +1,16 @@
 package com.vmware.nimbus.ui.main.fragments;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 
 import com.vmware.nimbus.R;
 import com.vmware.nimbus.api.APIService;
@@ -17,6 +23,7 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -38,6 +45,9 @@ public class DeploymentsViewFragment extends Fragment {
 
     private DeploymentsAdapter rvAdapter;
     private List<DeploymentItemModel.DeploymentItem> deploymentList;
+
+    private SearchView.OnQueryTextListener queryTextListener;
+    private SearchView searchView = null;
 
     /**
      * Creates a new instance of the view fragment.
@@ -67,7 +77,7 @@ public class DeploymentsViewFragment extends Fragment {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
         pageViewModel.setIndex(index);
-
+        setHasOptionsMenu(true);
     }
 
     /**
@@ -137,5 +147,35 @@ public class DeploymentsViewFragment extends Fragment {
                 mRecyclerView.setItemAnimator(new DefaultItemAnimator());
             }
         }, getContext());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+            queryTextListener = new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    rvAdapter.getFilter().filter(newText);
+                    return true;
+                }
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
+            };
+
+            searchView.setOnQueryTextListener(queryTextListener);
+            searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        }
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
