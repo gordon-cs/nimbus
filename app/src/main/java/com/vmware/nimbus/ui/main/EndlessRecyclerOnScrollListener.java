dfb9ad39e852394a4f0f8a1a5045aba4b8981ac8
kdeepshikha <kdeepshikha@vmware.com>
2021-06-15 17:00:15 -0400

    Adding pagination for loading deatils on scroll event for blueprint and deployment tab

---
+++ app/src/main/java/com/vmware/nimbus/ui/main/EndlessRecyclerOnScrollListener.java
@@ -0,0 +1,56 @@
+package com.vmware.nimbus;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
+
+    private int previousTotal = 0; // The total number of items in the dataset after the last load
+    private boolean loading = true; // True if we are still waiting for the last set of data to load.
+    private int visibleThreshold = 5; // The minimum amount of items to have below your current scroll position before loading more.
+    int firstVisibleItem, visibleItemCount, totalItemCount;
+
+    private int current_page = 0;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager) {
+        this.mLinearLayoutManager = linearLayoutManager;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        visibleItemCount = recyclerView.getChildCount();
+        totalItemCount = mLinearLayoutManager.getItemCount();
+        firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+
+        if (loading) {
+            if (totalItemCount > previousTotal) {
+                loading = false;
+                previousTotal = totalItemCount;
+            }
+        }
+        // Handling the scenario where item count is referring to the searched
+        // items which will always be less or equal than the previous total
+        if (!loading && (totalItemCount < previousTotal)) {
+            totalItemCount = previousTotal;
+            return;
+        }
+        if (!loading && (totalItemCount - visibleItemCount)
+                <= (firstVisibleItem + visibleThreshold)) {
+            current_page++;
+            onLoadMore(current_page);
+            loading = true;
+        }
+    }
+
+    public abstract void onLoadMore(int current_page);
+
+    public void resetState() {
+        this.current_page = 0;
+        this.previousTotal = 0;
+        this.loading = true;
+    }
+}
